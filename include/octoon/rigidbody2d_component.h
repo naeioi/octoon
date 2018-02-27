39ef06f5c3d92026eb917f4bd68324591defb769
JunWang <jstzwj@aliyun.com>
2018-02-27 18:23:44 +0800

    init

---
+++ include/octoon/rigidbody2d_component.h
@@ -0,0 +1,16 @@
+#ifndef OCTOON_RIGIDBODY2D_COMPONENT_H_
+#define OCTOON_RIGIDBODY2D_COMPONENT_H_
+
+#include <octoon/game_component.h>
+
+namespace octoon
+{
+    class OCTOON_EXPORT Rigidbody2DComponent final : public GameComponent
+	{
+
+
+    };
+}
+
+
+#endif // OCTOON_RIGIDBODY2D_COMPONENT_H_
---
+++ source/octoon/physic2d_feature.cpp
@@ -0,0 +1,36 @@
+#include <octoon/physic2d_feature.h>
+
+
+namespace octoon
+{
+    void Physic2DFeature::on_activate() except override
+    {
+
+    }
+
+    void Physic2DFeature::on_deactivate() noexcept override
+    {
+
+    }
+
+    void Physic2DFeature::on_input_event(const input::InputEvent& event) noexcept override
+    {
+
+    }
+
+    void Physic2DFeature::on_reset() noexcept override
+    {
+
+    }
+
+    void Physic2DFeature::on_frame_begin() noexcept override
+    {
+
+    }
+
+    void Physic2DFeature::on_frame_end() noexcept override
+    {
+
+    }
+
+}
