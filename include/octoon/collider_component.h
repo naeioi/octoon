c53cdfea7f9735153a3fcd4d5920b1cced0a5f99
WangJun <1103870790@qq.com>
2018-06-11 17:14:38 +0800

    add rigidbody

--- include/octoon/collider_component.h
+++ include/octoon/collider_component.h
@@ -5,13 +5,7 @@
 #include <memory>
 #include <octoon/game_component.h>
 #include <octoon/math/math.h>
-#include <octoon/physics_material.h>
-#include <octoon/collision.h>
 
-namespace physx
-{
-	class PxShape;
-}
 
 namespace octoon
 {
@@ -22,20 +16,20 @@ namespace octoon
 		Collider() noexcept;
 		~Collider();
 
-		void setSharedMaterial(PhysicsMaterial material) except;
-		std::shared_ptr<PhysicsMaterial> getSharedMaterial() except;
+		//void setSharedMaterial(PhysicsMaterial material) except;
+		//std::shared_ptr<PhysicsMaterial> getSharedMaterial() except;
 
-		physx::PxShape* getShape() noexcept { return shape; }
-
-	protected:
-		virtual void OnCollisionEnter(Collision collision); // OnCollisionEnter is called when this collider / rigidbody has begun touching another rigidbody / collider.
-		virtual void OnCollisionExit(Collision collision); // OnCollisionExit is called when this collider / rigidbody has stopped touching another rigidbody / collider.
-		virtual void OnCollisionStay(Collision collision); // OnCollisionStay is called once per frame for every collider / rigidbody that is touching rigidbody / collider.
+		//physx::PxShape* getShape() noexcept { return shape; }
 
 	protected:
-		physx::PxShape* shape;
+		//virtual void OnCollisionEnter(Collision collision); // OnCollisionEnter is called when this collider / rigidbody has begun touching another rigidbody / collider.
+		//virtual void OnCollisionExit(Collision collision); // OnCollisionExit is called when this collider / rigidbody has stopped touching another rigidbody / collider.
+		//virtual void OnCollisionStay(Collision collision); // OnCollisionStay is called once per frame for every collider / rigidbody that is touching rigidbody / collider.
 
-		std::shared_ptr<PhysicsMaterial> shared_material; // The PhysicsMaterial that is applied to this collider.
+	protected:
+		//physx::PxShape* shape;
+
+		//std::shared_ptr<PhysicsMaterial> shared_material; // The PhysicsMaterial that is applied to this collider.
 
 		friend class Rigidbody;
 	};
---
+++ include/octoon/rigidbody_component.h
@@ -0,0 +1,84 @@
+#ifndef OCTOON_RIGIDBODY_COMPONENT_H_
+#define OCTOON_RIGIDBODY_COMPONENT_H_
+
+#include <memory>
+#include <vector>
+#include <octoon/game_component.h>
+#include <octoon/physics_feature.h>
+#include <octoon/collider_component.h>
+#include <octoon/math/math.h>
+#include <octoon/runtime/singleton.h>
+
+namespace octoon
+{
+    enum class RigidbodySleepMode:int
+    {
+        NeverSleep, //Rigidbody never automatically sleeps.
+        StartAwake, //Rigidbody is initially awake.
+        StartAsleep, //Rigidbody is initially asleep.
+    };
+
+    class OCTOON_EXPORT Rigidbody final : public GameComponent
+	{
+		OctoonDeclareSubClass(Rigidbody, runtime::RttiInterface)
+        public:
+            Rigidbody() noexcept;
+			Rigidbody(bool type) noexcept;
+			Rigidbody(bool type, float mass) noexcept;
+			Rigidbody(bool type, float mass, const math::Vector3& offset) noexcept;
+            ~Rigidbody();
+            virtual GameComponentPtr clone() const noexcept;
+
+            void setAngularVelocity(float v) noexcept;
+            float getAngularVelocity() const noexcept;
+
+            void setGravityScale(float scale) noexcept;
+            float getGravityScale() const noexcept;
+
+            void setMass(float m) noexcept;
+            float getMass() const noexcept;
+
+			void setMassOffset(math::Vector3 offset) noexcept;
+			math::Vector3 getMassOffset() const noexcept;
+
+            void setSleepMode(RigidbodySleepMode mode) noexcept;
+            RigidbodySleepMode getSleepMode() const noexcept;
+
+			void setIsKinematic(bool type) noexcept;
+			bool getIsKinematic() const noexcept;
+
+			physx::PxRigidActor* getRigidbody() noexcept { return body; }
+
+        private:
+        	virtual void onActivate() except override;
+            virtual void onDeactivate() noexcept override;
+
+            virtual void onAttachComponent(const GameComponentPtr& component) noexcept;
+            virtual void onDetachComponent(const GameComponentPtr& component) noexcept;
+
+			virtual void onFrameBegin() except override;
+			virtual void onFrame() except override;
+			virtual void onFrameEnd() except override;
+
+			virtual void onMoveAfter() noexcept;
+
+			void buildRigidBody() except;
+			void releaseRigidBody() noexcept;
+        private:
+			physx::PxRigidActor* body;
+
+            float angularVelocity;
+            float gravityScale; // The degree to which this object is affected by gravity.
+			bool isKinematic;
+            float mass; // Mass of the rigidbody.
+			math::Vector3 massOffset;
+            RigidbodySleepMode sleepMode;
+            math::Vector3 velocity;
+
+            friend class Collider;
+            friend class BoxCollider;
+			friend class SphereCollider;
+    };
+}
+
+#endif // OCTOON_RIGIDBODY_COMPONENT_H_
